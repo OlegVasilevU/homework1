ce56d6c5d69749a64201b7a5d0bc5f86293d7f64
OlegVasilevU <trade.vasilev@yandex.ru>
2024-06-28 18:49:07 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,7 @@
+# Практическое задание по уроку "Строки и индексация строк"
+example = 'Противостояние'
+print(example[0])
+print(example[-1])
+print(example[7:])
+print(example[::-1])
+print(example[1:14:2])
